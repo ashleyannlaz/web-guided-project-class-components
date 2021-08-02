42fe08ba8c67ba30efdabfc82c7707f8458d22fc
ashleyannlaz <paperpastels@gmail.com>
2021-08-02 11:29:08 -0400

    Lecture

---
+++ review/src/App2.js
@@ -0,0 +1,53 @@
+import React from 'react'
+
+// const Person = (props) => {
+//     return(
+//     <div>
+//     <h1>Hello, {props.name}!</h1>
+//     <h2>Are you {props.age}?</h2>
+//     </div>
+//     );
+// }
+
+class Person extends React.Component {
+    render(){
+            return(
+            <div>
+            <h1>Hello, {this.props.name}!</h1>
+            <h2>Are you {this.props.age}?</h2>
+            </div>
+            );
+    }
+}
+
+class App2 extends React.Component {
+    constructor() { 
+        super();
+        this.state = {
+            name: 'Ashley',
+            age: 29
+        }
+    }
+    handleClick = () => {
+        this.setState({
+            ...this.state,
+            // ensures it keeps all old values
+            name: 'Emma',
+            age: 3
+        }
+        )
+    }
+
+    render () {
+        const name = 'Ashley';
+        return(
+            <div>
+                <Person name={this.state.name} age={this.state.age} />
+                <button onClick={this.handleClick}>Change the name.</button>
+            </div>
+        )
+    }
+    
+}
+
+export default App2;
